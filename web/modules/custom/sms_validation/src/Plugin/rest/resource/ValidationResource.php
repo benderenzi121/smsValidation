50da96932ce9813a741d80d3c9b73c018bfa63ab
Ben Derenzi <benderenzi@Bens-MacBook-Pro.local>
2023-02-13 12:46:25 -0500

    fix lint / Logger

--- web/modules/custom/sms_validation/src/Plugin/rest/resource/ValidationResource.php
+++ web/modules/custom/sms_validation/src/Plugin/rest/resource/ValidationResource.php
@@ -1,15 +1,13 @@
 <?php
+
 namespace Drupal\sms_validation\Plugin\rest\resource;
 
 use Drupal\rest\Plugin\ResourceBase;
 use Drupal\rest\ResourceResponse;
-use Tobscure\JsonApi\Collection;
 use Tobscure\JsonApi\Document;
-use Tobscure\JsonApi\ErrorHandler;
-use Tobscure\JsonApi\Resource;
 
 /**
- * Provides a Demo Resource
+ * Provides a Demo Resource.
  *
  * @RestResource(
  *   id = "validation_resource",
@@ -19,66 +17,68 @@ use Tobscure\JsonApi\Resource;
  *   }
  * )
  */
+class ValidationResource extends ResourceBase {
 
-class ValidationResource extends ResourceBase
-{
-    /**
-     * Responds to entity GET requests.
-     * 
-     * @return \Drupal\rest\ResourceResponse
-     */
-    public function get($phoneNumber) 
-    {
-        // Initialize our Twilio service object
-        $twilio_connector_service = \Drupal::Service(id: 'twilio.api_connector');
-        // Makes a request through twilio SDK. 
-        // Will return an empty array if the request could not be made
-        $verifyNumber = $twilio_connector_service->verifyNumber($phoneNumber);
-       
-        if (!empty($verifyNumber)) {
-            $phone_number_data = [
-                'valid' => $verifyNumber->valid,
-                'countryCode' => $verifyNumber->countryCode,
-              ];
-            if ($phone_number_data['valid'] == false) {
-                $document = new Document();
-                $document->setErrors(
-                    [
-                    'status' => '400',
-                    "title" => 'Invalid Phone Number',
-                    "detail" => 'the provided number is not recognized as a valid phone number',
-                    ]
-                );
-                return new ResourceResponse($document->toArray(), 400);
-            } elseif ($phone_number_data['countryCode'] != 'US') {
-                $document = new Document();
-                $document->setErrors(
-                    [
-                    "status" => '400',
-                    "title" => 'Invalid Phone Number',
-                    "detail" => 'the provided number must must reside in the US',
-                    ]
+  /**
+   * Responds to entity GET requests.
+   *
+   * @return \Drupal\rest\ResourceResponse
+   *   Returns a response based on whether the number is valid.
+   */
+  public function get($phoneNumber) {
+    // Initialize our Twilio service object.
+    $twilio_connector_service = \Drupal::Service(id: 'twilio.api_connector');
+    // Makes a request through twilio SDK.
+    // Will return an empty array if the request could not be made.
+    $verifyNumber = $twilio_connector_service->verifyNumber($phoneNumber);
+
+    if (!empty($verifyNumber)) {
+      $phone_number_data = [
+        'valid' => $verifyNumber->valid,
+        'countryCode' => $verifyNumber->countryCode,
+      ];
+      if ($phone_number_data['valid'] == FALSE) {
+        $document = new Document();
+        $document->setErrors(
+              [
+                'status' => '400',
+                "title" => 'Invalid Phone Number',
+                "detail" => 'the provided number is not recognized as a valid phone number',
+              ]
+          );
+        return new ResourceResponse($document->toArray(), 400);
+      }
+      elseif ($phone_number_data['countryCode'] != 'US') {
+        $document = new Document();
+        $document->setErrors(
+              [
+                "status" => '400',
+                "title" => 'Invalid Phone Number',
+                "detail" => 'the provided number must must reside in the US',
+              ]
                 );
 
-                return new ResourceResponse($document->toArray(), 400);
-            }
-            // Calls to the verify SMS function of the twilio service.
-            // this is a paid request that returns mobile carrier information
-            $result = $twilio_connector_service->verifySms($phoneNumber);
-            if ($result === 'mobile') {
-                return new ResourceResponse(202);
-            } else {
-                $document = new Document();
-                $document->setErrors(
-                    [
-                    "status" => '400',
-                    "title" => 'Invalid Phone Number',
-                    "detail" => 'the provided number must be a valid mobile number to recieve sms',
-                    ]
+        return new ResourceResponse($document->toArray(), 400);
+      }
+      // Calls to the verify SMS function of the twilio service.
+      // this is a paid request that returns mobile carrier information.
+      $result = $twilio_connector_service->verifySms($phoneNumber);
+      if ($result === 'mobile') {
+        return new ResourceResponse(202);
+      }
+      else {
+        $document = new Document();
+        $document->setErrors(
+              [
+                "status" => '400',
+                "title" => 'Invalid Phone Number',
+                "detail" => 'the provided number must be a valid mobile number to recieve sms',
+              ]
                 );
-                  return new ResourceResponse($document->toArray(), 500);
-            }
-        }
-        return new ResourceResponse($verifyNumber);
+        return new ResourceResponse($document->toArray(), 500);
+      }
     }
+    return new ResourceResponse($verifyNumber);
+  }
+
 }
--- web/modules/custom/sms_validation/src/Service/TwilioAPIConnector.php
+++ web/modules/custom/sms_validation/src/Service/TwilioAPIConnector.php
@@ -1,90 +1,83 @@
 <?php
-    namespace Drupal\sms_validation\Service;
 
-    use Drupal;
-    use Symfony\Component\HttpFoundation\Response;
-    use Twilio\Exceptions\TwilioException;
-    use Twilio\Rest\Client;
-    use Drupal\Core\Site\Settings;
-    use Symfony\Component\HttpFoundation\JsonResponse;
+namespace Drupal\sms_validation\Service;
 
-    class TwilioAPIConnector{
-        
-    /**
-     * Twilio  client variable;.
-     *
-     * @var /Twilio\Rest\Client
-     */    
-    protected $twilioClient;
+use Twilio\Exceptions\TwilioException;
+use Twilio\Rest\Client;
+use Drupal\Core\Site\Settings;
+use Drupal\core\http\ClientFactory;
+use GuzzleHttp\Exception\RequestException\GuzzleHttp\Exception\RequestException;
+use Drupal;
 
-     /**
-     * Constructor function that creats an instance of our twilio client
-     *  based on the SID and AuthToken in our settings.php file
-     * 
-     */    
+/**
+ * Twilio connector class.
+ */
+class TwilioAPIConnector {
+  /**
+   * Twilio  client variable;.
+   *
+   * @var Twilio\Rest\Client
+   */
+  protected $twilioClient;
 
-    public function __construct(\Drupal\core\http\ClientFactory $client)
-    {
-        $sid = Settings::get('twilio_sid');
-        $token = Settings::get('twilio_auth');
-
-        $this->twilioClient = new Client($sid, $token);
+  /**
+   * Constructor function that creats an instance of our twilio client.
+   */
+  public function __construct(ClientFactory $client) {
+    // Test API does not send out SMS.
+    if (Settings::get('environment') === 'dev') {
+      $sid   = Settings::get('test_twilio_sid');
+      $token = Settings::get('test_twilio_token');
+    }
+    else {
+      $sid   = Settings::get('twilio_sid');
+      $token = Settings::get('twilio_token');
     }
 
-    /**
-     * Our Verify Number function hits the twilio API to lookup a phone number
-     *  
-     * As long as the request from twilio comes back, it will return the data provided by twillio. 
-     * @param string $phone_number
-     * 
-     */
+    $this->twilioClient = new Client($sid, $token);
+  }
 
-    public function verifyNumber($phone_number)
-    {
-        // Initialize an empty array that our data will eventually go into
-        $data = [];
+  /**
+   * Our Verify Number function hits the twilio API to lookup a phone number.
+   *
+   * @param string $phone_number
+   *   Telephone Number to check.
+   */
+  public function verifyNumber($phone_number) {
+    // Initialize an empty array that our data will eventually go into.
+    $data = [];
 
-        /**
-        *   Try to hit the twilio endpoint with the phone number provided
-        *       will populate our data array if there is a response from the twilio api
-        *       
-        *       If there is no response we return an array with nothing inside of it 
-        *           and throow an exception
-        */
-        try {
-            $validation = $this->twilioClient->lookups->v2->phoneNumbers($phone_number)->fetch();
-            $data = $validation;
-        }
-        catch (TwilioException $e) {
-            throw $e;
-        }
-        catch(\GuzzleHttp\Exception\RequestException $e){
-            watchdog_exception('twilio', $e, $e->getMessage());
-        }
-
-        return $data;
+    // Try to hit the twilio endpoint with the phone number provided.
+    try {
+      $validation = $this->twilioClient->lookups->v2->phoneNumbers($phone_number)->fetch();
+      $data = $validation;
     }
-
-     /**
-      *  VerifySms function is used to verify that the number provided is a valid mobile number
-      *      from within the US. 
-      * 
-      * @param string $phone_number
-      *
-      */
-    public function verifySms($phone_number)
-    {
-        try {
-            $validation = $this->twilioClient->lookups->v1->phoneNumbers($phone_number)->fetch(["type" => ["carrier"]]);
-        }
-        catch (TwilioException $e) {
-            throw $e;
-        }
-        catch(\GuzzleHttp\Exception\RequestException $e){
-            watchdog_exception('twilio', $e, $e->getMessage());
-        }
-        return $validation->carrier['type'];
+    catch (TwilioException $e) {
+      throw $e;
     }
+    catch (\GuzzleHttp\Exception\RequestException $e) {
+      Drupal::logger('twilio', $e, $e->getMessage());
+    }
+    return $data;
+  }
+
+  /**
+   * VerifySms is used to verify that the number is a valid mobile number.
+   *
+   * @param string $phone_number
+   *   Phone Number to verify.
+   */
+  public function verifySms($phone_number) {
+    try {
+      $validation = $this->twilioClient->lookups->v1->phoneNumbers($phone_number)->fetch(["type" => ["carrier"]]);
+    }
+    catch (TwilioException $e) {
+      throw $e;
+    }
+    catch (RequestException $e) {
+      Drupal::logger('twilio', $e, $e->getMessage());
+    }
+    return $validation->carrier['type'];
+  }
 
 }
-?> 
